7605a5b8806c1b010b98436f4e016248dc572b10
aihaoyun <145974662+aihaoyun@users.noreply.github.com>
2023-10-07 23:53:24 +0800

    Add files via upload

--- javascript/load_easy_ui.js
+++ javascript/load_easy_ui.js
@@ -1,1 +1,7 @@
-
+setTimeout(function() {
+    const t2iapi = document.getElementById("tab_t2iapi");
+    const t2iapi_div = document.createElement("div");
+    t2iapi_div.style = "height:700px";
+    t2iapi_div.innerHTML = '<iframe id="t2iapiIframe" name="Frame" src="/t2iapi/easy_ui.html" style="width: 100%; height: 100%;"></iframe>';
+    t2iapi.appendChild(t2iapi_div);
+},1000*25);
